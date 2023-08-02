8893b69862e8407a4efbc2888b66761a652e7059
Sibongiseni Mthetwa <137326611+sibongiseni1@users.noreply.github.com>
2023-08-02 17:17:16 +0200

    Update Average_Marks_calculator.cpp

--- Average_Marks_calculator.cpp
+++ Average_Marks_calculator.cpp
@@ -7,21 +7,21 @@ int main()
     double sum = 0;
     double mark;
     double average;
-    int counter = 0;
+    int count = 0;
 
     for(int k =0; k < 5; k++){
         cout << "Enter the student's mark " << (k + 1) << ": ";
         cin >> mark;
 
         if(mark >= 75){
-            counter++;
+            count++;
         }
         sum += mark;
     }
 
     average = sum / 5.0;
     cout << "The average is: " << average << endl;
-    cout << "A total of " << counter << " distinctions!" << endl;
+    cout << "A total of " << count << " distinctions!" << endl;
 
     return 0;
 }
